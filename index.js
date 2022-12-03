8b0e084932b502f01c2d5e1ea47cb57435392779
SAMUEL-UME <104633362+SAMUEL-UME@users.noreply.github.com>
2022-12-03 11:45:56 +0100

    New changes

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const socket = require("socket.io")
+const socket = require("socket.io");
 
 const app = express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my home page");
+});
 
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRoutes);
