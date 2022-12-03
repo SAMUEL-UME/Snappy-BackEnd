d290611c5ae90630bc2779e598b9322d3515bffe
SAMUEL-UME <104633362+SAMUEL-UME@users.noreply.github.com>
2022-12-03 07:52:16 +0100

    Fixed cors errores

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const socket = require("socket.io")
+const socket = require("socket.io");
 
 const app = express();
 require("dotenv").config();
@@ -11,10 +11,13 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
+app.get("/", (req, res) => {
+  res.send("Welcome to my home page");
+});
 
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRoutes);
